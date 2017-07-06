369e5c7d1ee152fc569c0d0ed32844764fd46c5d
simongreene02 <simongreene02@gmail.com>
2017-07-05 22:14:52 -0500

    Almost finished click script.

---
+++ Assets/Potion Scripts/ClickHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+
+	private bool isMouseOn;
+	[HideInInspector]
+	public ClickHandler[] childScripts;
+
+	// Use this for initialization
+	void Start () {
+		childScripts = new ClickHandler[this.gameObject.transform.childCount];
+		for (int i = 0; i < childScripts.Length; i++) {
+			childScripts [i] = this.gameObject.transform.GetChild (i).gameObject.AddComponent<ClickHandler> () as ClickHandler;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.parent == null) {
+			print (IsMouseOn());
+		}
+	}
+
+	void OnMouseEnter () {
+		isMouseOn = true;
+	}
+
+	void OnMouseExit () {
+		isMouseOn = false;
+	}
+
+	public bool IsMouseOn() {
+		if (isMouseOn) {
+			return true;
+		}
+		for (int i = 0; i < childScripts.Length; i++) {
+			if (childScripts [i].IsMouseOn()) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
