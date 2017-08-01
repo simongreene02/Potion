477833de85368d405dff4db63da2502aa28380e3
simongreene02 <simongreene02@gmail.com>
2017-07-31 20:22:08 -0500

    Added collider to tree.

--- Assets/Potion Scripts/GroveScripts/LevitateGround.cs
+++ Assets/Potion Scripts/GroveScripts/LevitateGround.cs
@@ -14,6 +14,7 @@ public class LevitateGround : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (Mathf.Sqrt (Mathf.Pow (player.transform.position.x - tree.transform.position.x, 2) + Mathf.Pow (player.transform.position.z - tree.transform.position.z, 2)));
+		float distance = Mathf.Sqrt (Mathf.Pow (player.transform.position.x - tree.transform.position.x, 2) + Mathf.Pow (player.transform.position.z - tree.transform.position.z, 2));
+		this.gameObject.transform.position = new Vector3 (this.gameObject.transform.position.x, 85 - distance / 3, this.gameObject.transform.position.y);
 	}
 }
