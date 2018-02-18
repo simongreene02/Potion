25c30a83d98923cd95dfaa10a7d9199cbf092cfb
simongreene02 <simongreene02@gmail.com>
2018-02-17 22:38:47 -0600

    Updated .gitignore

---
+++ Assets/Potion Scripts/BarScripts/BackAndForthScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackAndForthScript : MonoBehaviour {
+
+	public Vector3 position1;
+	public Vector3 position2;
+	public float travelTime = 1.0f;
+	private float timeRunning;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRunning += Time.deltaTime;
+		if ((timeRunning / travelTime) % 2 < 1) {
+			this.gameObject.transform.position = new Vector3 (Mathf.Lerp (position1.x, position2.x, (timeRunning % travelTime) / travelTime), Mathf.Lerp (position1.y, position2.y, (timeRunning % travelTime) / travelTime), Mathf.Lerp (position1.z, position2.z, (timeRunning % travelTime) / travelTime));
+		} else {
+			this.gameObject.transform.position = new Vector3 (Mathf.Lerp (position2.x, position1.x, (timeRunning % travelTime) / travelTime), Mathf.Lerp (position2.y, position1.y, (timeRunning % travelTime) / travelTime), Mathf.Lerp (position2.z, position1.z, (timeRunning % travelTime) / travelTime));
+		}
+	}
+}
--- Assets/Potion Scripts/BeachScripts/WaveRotation.cs
+++ Assets/Potion Scripts/BeachScripts/WaveRotation.cs
@@ -7,9 +7,11 @@ public class WaveRotation : MonoBehaviour {
 	private float rotationAmount = 0f;
 	public double secondsPerRotation = 2;
 	public double circleLength = 1.0;
+	public double initialAngle;
 
 	// Use this for initialization
 	void Start () {
+		rotationAmount = (float) initialAngle / 180;
 		this.gameObject.transform.position += new Vector3 ((float) (circleLength * Mathf.Sin (Mathf.PI * rotationAmount)), (float) (circleLength * Mathf.Cos (Mathf.PI * rotationAmount)), 0);
 	}
 	
