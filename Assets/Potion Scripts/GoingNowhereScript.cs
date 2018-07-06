fcccc7ea7e6a8700a998a721b0861505cbd5afdd
simongreene02 <simongreene02@gmail.com>
2018-07-05 21:34:04 -0400

    Added clickable "going nowhere" signs.

---
+++ Assets/Potion Scripts/GoingNowhereScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoingNowhereScript : MonoBehaviour, ActivateOnClickScript {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnBeingClicked () {
+		FlagHandler.SetItem ("carryingBasket", 0);
+		FlagHandler.ChangeScene ("Potion - Cabin v1.6");
+	}
+}
